7c2c8c989273bb276c088d31fbf44d1db57a2759
JettPlay1 <jettplayyt@gmail.com>
2024-05-12 18:33:22 +0500

    Using .env vars

--- db/init.sql
+++ db/init.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS phone_numbers(
 INSERT INTO emails (email) VALUES ('test@test.com'), ('test2@test2.ru');
 INSERT INTO phone_numbers (phone_number) VALUES ('88005553535'), ('+7 987 654 32 10');
 
-CREATE USER repl_user WITH REPLICATION ENCRYPTED PASSWORD 'qwerty123';
+CREATE USER ${DB_REPL_USER} WITH REPLICATION ENCRYPTED PASSWORD ${DB_REPL_PASSWORD};
 SELECT pg_create_physical_replication_slot('replication_slot');
 
 CREATE USER tg_bot_user WITH PASSWORD 'qwerty123';
